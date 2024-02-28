6da2ad933e29326a437a9f1f2543d3e0b4b79541
SaadHussein <sa3dhusseein@gmail.com>
2024-02-28 15:54:47 +0200

    fix bug in create Job and add more security middleware in app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -10,10 +16,19 @@ const connectDB = require('./db/connect');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
+app.use(
+  rateLimiter({
+    windowMs: 1000 * 60 * 15,
+    max: 100
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API</h1>');
 });
 
 app.use('/api/v1/auth', authRouter);
